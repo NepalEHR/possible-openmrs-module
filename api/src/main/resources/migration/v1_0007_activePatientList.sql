1e59dd54b23226c979a2b33f2b6e9671e9202395
Dipak Thapa <dipakthapaofficial@gmail.com>
2018-09-10 19:28:25 +0545

    Fixes admitted tab patient list

--- api/src/main/resources/migration/v1_0007_activePatientList.sql
+++ api/src/main/resources/migration/v1_0007_activePatientList.sql
@@ -10,7 +10,9 @@ VALUES ('emrapi.sqlSearch.activePatients',
           pi.identifier as identifier,
           concat("",p.uuid) as uuid,
           concat("",v.uuid) as activeVisitUuid,
-          IF(va.value_reference = "Admitted", "true", "false") as hasBeenAdmitted
+          IF(va.value_reference = "Admitted", "true", "false") as hasBeenAdmitted,
+		  b.bed_number AS bedNumber,
+		  bt.display_name AS bedType
         from visit v
         join person_name pn on v.patient_id = pn.person_id and pn.voided = 0 AND pn.preferred= 1
         join patient_identifier pi on v.patient_id = pi.patient_id and pi.preferred = 1 and pi.voided = 0
@@ -18,6 +20,9 @@ VALUES ('emrapi.sqlSearch.activePatients',
         join global_property gp on gp.property="bahmni.primaryIdentifierType" and gp.property_value=pit.uuid
         join person p on p.person_id = v.patient_id
         join location l on l.uuid = ${visit_location_uuid} and v.location_id = l.location_id
+		join bed_patient_assignment_map bpam ON bpam.patient_id = pn.person_id
+        join bed b ON b.bed_id = bpam.bed_id
+		join bed_type bt ON bt.bed_type_id = b.bed_type_id
         left outer join visit_attribute va on va.visit_id = v.visit_id and va.attribute_type_id = (
           select visit_attribute_type_id from visit_attribute_type where name="Admission Status"
         ) and va.voided = 0
---
+++ api/src/main/resources/migration/v1_0008_admittedPatientList.sql
@@ -0,0 +1,34 @@
+DELETE FROM global_property
+WHERE property IN (
+  'emrapi.sqlSearch.admittedPatients'
+);
+
+INSERT INTO global_property (property,property_value,description,uuid) 
+	VALUES ('emrapi.sqlSearch.admittedPatients',
+            'SELECT DISTINCT
+    CONCAT(pn.given_name, \' \', pn.family_name) AS name,
+    pi.identifier AS identifier,
+    CONCAT("", p.uuid) AS uuid,
+    CONCAT("", v.uuid) AS activeVisitUuid,
+    IF(va.value_reference = "Admitted", "true", "false") AS hasBeenAdmitted,
+    b.bed_number AS bedNumber,
+    bt.display_name AS bedType
+	FROM
+    	visit v
+    JOIN person_name pn ON v.patient_id = pn.person_id AND pn.voided = 0
+    JOIN patient_identifier pi ON v.patient_id = pi.patient_id
+    JOIN patient_identifier_type pit ON pi.identifier_type = pit.patient_identifier_type_id
+    JOIN global_property gp ON gp.property = "bahmni.primaryIdentifierType" AND gp.property_value = pit.uuid
+    JOIN person p ON v.patient_id = p.person_id
+    JOIN visit_attribute va ON v.visit_id = va.visit_id AND va.value_reference = "Admitted" AND va.voided = 0
+    JOIN visit_attribute_type vat ON vat.visit_attribute_type_id = va.attribute_type_id
+        AND vat.name = "Admission Status"
+    JOIN bed_patient_assignment_map bpam ON bpam.patient_id = pn.person_id
+    JOIN bed b ON b.bed_id = bpam.bed_id
+    JOIN location l on l.uuid = ${visit_location_uuid} and v.location_id = l.location_id
+    JOIN bed_type bt ON bt.bed_type_id = b.bed_type_id
+	WHERE
+    v.date_stopped IS NULL AND v.voided = 0',
+    'SQL Query to get admittedPatients',
+    uuid()
+);
